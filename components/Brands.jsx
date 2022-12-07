ce4776cf5a7ca379879db756306fd29036697e67
furkanyucel <furkanyucelux@gmail.com>
2022-12-07 18:01:26 +0300

    updated

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -5,7 +5,9 @@ const Brands = () => {
   return (
     <div className='text-center py-[90px]'>
       <h2 className='w-[650px] mx-auto font-bold text-5xl leading-[56px]'>We are blessed to work with leading brands.</h2>
-      <p className='mt-8 font-bold text-[16px] text-blue-500'>View Work</p>
+      <p className='mt-8 font-bold text-[16px] text-blue-500 hover:text-blue-600 hover:underline'>
+        <a href="#">View Work</a>
+      </p>
       <div className="mt-[80px] flex justify-center text-5xl gap-x-14 text-slate-300">
         <TbBrandAirbnb />
         <TbBrandAppleArcade />
