b8e104e6516f0f5f179aa8b37c685d3794e318ea
furkanyucel <furkanyucelux@gmail.com>
2022-12-08 17:42:47 +0300

    updated

---
+++ components/Reviews.jsx
@@ -0,0 +1,191 @@
+import React from "react";
+import { FaStar } from "react-icons/fa";
+import Avatar from "react-avatar";
+
+const Reviews = () => {
+  return (
+    <div className="cont py-[90px] bg-slate-100">
+      <h1 className="w-[600px] mx-auto font-bold text-5xl leading-[56px] text-slate-900 text-center">
+        Our clients feedback is important for us.
+      </h1>
+
+      <div className="mt-[80px] flex flex-wrap gap-14">
+        <div className="p-12 bg-slate-200 hover:scale-105 transition-all hover:shadow-md">
+          <div className="mb-8 flex gap-x-3 text-yellow-400">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+
+          <div>
+            <p className="w-[240px] font-normal text-sm">
+              "The future belongs to those who learn more skills and combine
+              them in creative ways."
+            </p>
+          </div>
+
+          <div className="border-[0.5px] border-slate-300 my-8"></div>
+
+          <div className="flex items-center">
+            <Avatar name="Furkan" size="60" className="rounded-full" />
+            <div className="ml-4">
+              <h4 className="font-medium text-base text-slate-900">
+                Angel Uriostegui
+              </h4>
+              <p className="font-normal text-sm text-slate-400">Powerkit</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-12 bg-slate-200 hover:scale-105 transition-all hover:shadow-md">
+          <div className="mb-8 flex gap-x-3 text-yellow-400">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+
+          <div>
+            <p className="w-[240px] font-normal text-sm">
+              "The future belongs to those who learn more skills and combine
+              them in creative ways."
+            </p>
+          </div>
+
+          <div className="border-[0.5px] border-slate-300 my-8"></div>
+
+          <div className="flex items-center">
+            <Avatar name="Furkan" size="60" className="rounded-full" />
+            <div className="ml-4">
+              <h4 className="font-medium text-base text-slate-900">
+                Angel Uriostegui
+              </h4>
+              <p className="font-normal text-sm text-slate-400">Powerkit</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-12 bg-slate-200 hover:scale-105 transition-all hover:shadow-md">
+          <div className="mb-8 flex gap-x-3 text-yellow-400">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+
+          <div>
+            <p className="w-[240px] font-normal text-sm">
+              "The future belongs to those who learn more skills and combine
+              them in creative ways."
+            </p>
+          </div>
+
+          <div className="border-[0.5px] border-slate-300 my-8"></div>
+
+          <div className="flex items-center">
+            <Avatar name="Furkan" size="60" className="rounded-full" />
+            <div className="ml-4">
+              <h4 className="font-medium text-base text-slate-900">
+                Angel Uriostegui
+              </h4>
+              <p className="font-normal text-sm text-slate-400">Powerkit</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-12 bg-slate-200 hover:scale-105 transition-all hover:shadow-md">
+          <div className="mb-8 flex gap-x-3 text-yellow-400">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+
+          <div>
+            <p className="w-[240px] font-normal text-sm">
+              "The future belongs to those who learn more skills and combine
+              them in creative ways."
+            </p>
+          </div>
+
+          <div className="border-[0.5px] border-slate-300 my-8"></div>
+
+          <div className="flex items-center">
+            <Avatar name="Furkan" size="60" className="rounded-full" />
+            <div className="ml-4">
+              <h4 className="font-medium text-base text-slate-900">
+                Angel Uriostegui
+              </h4>
+              <p className="font-normal text-sm text-slate-400">Powerkit</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-12 bg-slate-200 hover:scale-105 transition-all hover:shadow-md">
+          <div className="mb-8 flex gap-x-3 text-yellow-400">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+
+          <div>
+            <p className="w-[240px] font-normal text-sm">
+              "The future belongs to those who learn more skills and combine
+              them in creative ways."
+            </p>
+          </div>
+
+          <div className="border-[0.5px] border-slate-300 my-8"></div>
+
+          <div className="flex items-center">
+            <Avatar name="Furkan" size="60" className="rounded-full" />
+            <div className="ml-4">
+              <h4 className="font-medium text-base text-slate-900">
+                Angel Uriostegui
+              </h4>
+              <p className="font-normal text-sm text-slate-400">Powerkit</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-12 bg-slate-200 hover:scale-105 transition-all hover:shadow-md">
+          <div className="mb-8 flex gap-x-3 text-yellow-400">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+
+          <div>
+            <p className="w-[240px] font-normal text-sm">
+              "The future belongs to those who learn more skills and combine
+              them in creative ways."
+            </p>
+          </div>
+
+          <div className="border-[0.5px] border-slate-300 my-8"></div>
+
+          <div className="flex items-center">
+            <Avatar name="Furkan" size="60" className="rounded-full" />
+            <div className="ml-4">
+              <h4 className="font-medium text-base text-slate-900">
+                Angel Uriostegui
+              </h4>
+              <p className="font-normal text-sm text-slate-400">Powerkit</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
